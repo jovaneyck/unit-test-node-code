b9452a957c7cfe5257c41cd8a2cf0511a18d1cd0
Jo Van Eyck <jo.vaneyck@ae.be>
2020-05-08 16:11:20 +0200

    another module as index is not really interesting to test

--- src/greeter.spec.ts
+++ src/greeter.spec.ts
@@ -1,3 +1,5 @@
+import * as greeter from "./greeter";
+
 test("hello world", () => {
     expect(1+1).toBe(2);
 })
--- src/greeter.ts
+++ src/greeter.ts
@@ -1,1 +1,6 @@
-export const greet = () => "Hello world!!";
+import * as database from "./database";
+
+export const greet = () => {
+    const name = database.people[0];
+    return `Hello ${name}!`;
+}
